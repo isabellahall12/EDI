eb029c5e02f7ce4ae4123e1315b97971f052b359
isabellahall12 <90520473+isabellahall12@users.noreply.github.com>
2021-11-16 14:07:19 -0500

    background

---
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas (windowWidth, windowHeight);//this creates the window width and height to be the size of my browser window
+background (77,164,234)
+}
+
+function draw() {
+
+}
