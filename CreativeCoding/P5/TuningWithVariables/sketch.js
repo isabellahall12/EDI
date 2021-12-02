52f3621d2cbaf802ef74c4cb5eea29da1a916bd9
isabellahall12 <90520473+isabellahall12@users.noreply.github.com>
2021-12-02 09:45:04 -0500

    code updates

--- CreativeCoding/P5/TuningWithVariables/sketch.js
+++ CreativeCoding/P5/TuningWithVariables/sketch.js
@@ -1,19 +1,19 @@
 function setup() {//runs once
-  createCanvas(windowWidth, windowWidth);
+  createCanvas(windowWidth, windowHeight);
   fill('pink');
   strokeWeight (1);
 }
 
 function draw() {//runs in a loop
- 
-  
-  var num = 20; //varible for the number of squares in the array 
-  
+
+
+  var num = 20; //varible for the number of squares in the array
+
    var sideLen = windowWidth/num; //variable for the side length of each square
-  
+
   for(var y = 0; y < windowWidth; y = y + sideLen) { //loop to create a row of squares in the y direction
   for(var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x direction
-  
+
       quad(x,y,
       x+ sideLen,y,
       x+ sideLen,y+ sideLen,
@@ -21,5 +21,5 @@ function draw() {//runs in a loop
   }
 
 }
-  
+
 }
---
+++ CreativeCoding/P5/TuningWithVariables_GIF/sketch.js
@@ -0,0 +1,36 @@
+let gif;
+function preload() {
+  gif = loadImage('assets/AiBGsingle.png');
+}
+function setup() {
+  image(gif, 0, 0);
+}
+
+function setup() {//runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('pink');
+  strokeWeight (1);
+}
+
+function draw() {//runs in a loop
+
+
+  var num = 20; //varible for the number of squares in the array
+
+   var sideLen = windowWidth/num; //variable for the side length of each square
+
+  for(var y = 0; y < windowHeight; y = y + sideLen) { //loop to create a row of squares in the y direction
+  for(var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x direction
+
+
+image(gif,x,y);
+      
+  }
+
+}
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
